4e096884ddffbfb772402b98e1a0f1e6510edef5
bsharkey <bsharkey@waveapps.com>
2022-12-01 09:41:06 -0500

    asdff

--- addition.py
+++ addition.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
 print("test")
+print("test2")
+
+key = b'-8B key-'
+DES.new(key, DES.MODE_OFB) # Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
+
+num1 = 1
+num2 = 10
+
+num2 =- num1
+num2 =+ num1
+
+if num1 <> num2:
+    print("not equal")
