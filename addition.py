0a738b81ad2cb97265368b1854afda720911ddc4
bsharkey <bsharkey@waveapps.com>
2022-12-01 09:55:37 -0500

    asdfff

--- addition.py
+++ addition.py
@@ -14,3 +14,8 @@ num2 =+ num1
 
 if num1 <> num2:
     print("not equal")
+key = b'-8B key-'
+DES.new(key, DES.MODE_OFB) # Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
+
+key = DES3.adjust_key_parity(get_random_bytes(24))
+cipher = DES3.new(key, DES3.MODE_CFB) # Noncompliant: Triple DES is vulnerable to meet-in-the-middle attack
